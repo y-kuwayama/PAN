a7947625cda2323a2fe6cb76e6b023926043372b
桑山祐一 <kuwayamayuuichi@MacBook.local>
2016-03-27 17:21:44 +0900

    edit/update/homeの流れを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,12 @@ class UsersController < ApplicationController
   end
 
   def update
+    current_user.update(update_params)
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:family_name, :first_name)
   end
 
 end
